4c48455fd9fb4c9547d6bc16a5ddf2f3705f5a37
Glebk0 <glebko123@gmail.com>
2018-07-30 16:40:12 +0300

    task2 and task3 added

--- task2.groovy
+++ task2.groovy
@@ -1,17 +1,18 @@
+
 import hudson.model.FreeStyleProject
 import hudson.plugins.git.GitSCM
 import hudson.plugins.ws_cleanup.*
 import hudson.tasks.Shell
 import jenkins.model.Jenkins
 
-job = Jenkins.instance.createProject(FreeStyleProject, 'groovy-created-job')
+task2 = Jenkins.instance.createProject(FreeStyleProject, 'MNT-CD-module9-extcreated-task2')
 
 def url = "https://github.com/Glebk0/simple_webapp.git"
 def gitScm = new GitSCM(url)
 gitScm.branches = [new hudson.plugins.git.BranchSpec("*/master")]
-job.scm = gitScm
+task2.scm = gitScm
 
-job.buildersList.add(new Shell('Hello'))
-job.save()
+task2.buildersList.add(new Shell('Hello'))
+task2.save()
 
-job.buildWrappersList.add(new PreBuildCleanup(null, true, "", ""))
+task2.buildWrappersList.add(new PreBuildCleanup(null, true, "", ""))
